4f316be20fae1ecfe9ac2c4d985dcda1d8b7a984
Bappy56 <33536703+Bappy56@users.noreply.github.com>
2017-12-30 18:22:34 -0700

    Live 0.3.1 push
    Fixed crash due to missing prefix.

--- haste_bot_v0.3.1.js
+++ haste_bot_v0.3.1.js
@@ -3,8 +3,9 @@ const client = new Discord.Client();
 const config = require("./hasteconfig.json");
 const help = require("./hastehelp.json");
 const embed = new Discord.RichEmbed();
+var pre;
 
-const version = "Bot version: 0.1 ***Alpha***";
+const version = "Bot version: 0.3.1 ***Alpha***";
 
 var afterhours = false;
 
@@ -23,7 +24,8 @@ client.on("ready", () =>
 
 client.on("message", message =>
 {
-	const pre = message.guild.emojis.get("349604451281141762").toString().toLowerCase();
+	if(message.guild.name == "Haste") pre = message.guild.emojis.find("name", "HasteComet").toString().toLowerCase();
+	else pre = config.prefix;
 	// console.log(message.content);
     var text = message.content.toLowerCase();
 
@@ -33,12 +35,12 @@ client.on("message", message =>
     ==================================================================*/
 
     //Basic text commands
-	if(text.startsWith(pre+" ping")) message.channel.send("Pong! `"+(Date.now()-message.createdTimestamp)+" ms`");
+	if(text.startsWith(pre+" ping")) message.channel.send("Pong! Host Server -> Discord API Service Response Time: `"+(Date.now()-message.createdTimestamp)+" ms`");
 
     else if(text.startsWith(pre+" price"))
     {
         embed.setTitle("Here's Haste's current prices!");
-        embed.setAuthor(config.username, client.user.avatarURL);
+        embed.setAuthor("Haste Auto", client.user.avatarURL);
         embed.setColor(config.color);
         embed.setURL("https://haste.net/pricing/");
 
@@ -48,7 +50,7 @@ client.on("message", message =>
     else if(text.startsWith(pre+" support"))
     {
         embed.setTitle("Need help with Haste? Go here!");
-        embed.setAuthor(config.username, client.user.avatarURL);
+        embed.setAuthor("Haste Auto", client.user.avatarURL);
         embed.setColor(config.color);
         embed.setURL("https://support.haste.net/hc/en-us");
 
@@ -58,7 +60,7 @@ client.on("message", message =>
     else if(text.startsWith(pre+" status"))
     {
         embed.setTitle("Here's the current Haste Server status!");
-        embed.setAuthor(config.username, client.user.avatarURL);
+        embed.setAuthor("Haste Auto", client.user.avatarURL);
         embed.setColor(config.color);
         embed.setURL("http://status.haste.net/");
 
@@ -68,7 +70,7 @@ client.on("message", message =>
     else if(text.startsWith(pre+" survey"))
     {
         embed.setTitle("Got a game or region you would like to see supported by Haste? Submit it here!");
-        embed.setAuthor("Haste Test");
+        embed.setAuthor("Haste Auto", client.user.avatarURL);
         embed.setColor(config.color);
         embed.setURL("https://www.surveymonkey.com/r/HasteGameRegionInterest");
 
@@ -79,7 +81,7 @@ client.on("message", message =>
     {
         embed.setTitle("These are our currently supported games and regions!");
         embed.setURL("https://haste.net/games/");
-        embed.setAuthor("Haste Test");
+        embed.setAuthor("Haste Auto", client.user.avatarURL);
         embed.setColor(config.color);
 
         message.channel.send(embed);
@@ -89,7 +91,7 @@ client.on("message", message =>
     {
         embed.setTitle("Here's how to help reduce your ping!");
         embed.setURL("https://haste.net/2017/09/29/get-close-possible-lan-gaming-online/");
-        embed.setAuthor("Haste Test");
+        embed.setAuthor("Haste Auto", client.user.avatarURL);
         embed.setColor(config.color);
 
         message.channel.send(embed);
@@ -97,7 +99,7 @@ client.on("message", message =>
 
     else if(text.startsWith(pre+" commands"))
     {
-    	if(message.member.roles.find("name", "Haste Staff") || message.member.roles.find("name", "Community Moderator")) message.channel.send(help.adminCommands+"\nVersion: "+config.version);
+    	if(message.member.roles.find("name", "Haste Staff") || message.member.roles.find("name", "Community Moderator")) message.channel.send(help.adminCommands+"\nVersion: "+version);
         else message.channel.send(help.commands);
     }
 
@@ -122,7 +124,8 @@ client.on("message", message =>
 //Support channel listener (Replace channel ID with target channel's)
 client.on("message", message =>
 {
-	const pre = message.guild.emojis.get("349604451281141762").toString().toLowerCase();
+	if(message.guild.name == "Haste") pre = message.guild.emojis.find("name", "HasteComet").toString().toLowerCase();
+	else pre = config.prefix;
 	var text = message.content.toLowerCase();
 	const supportChan = message.guild.channels.find("name", "support").id;
 
@@ -138,70 +141,70 @@ client.on("message", message =>
 client.on("guildBanAdd", (banGuild, banUser) =>
 {
 	var banReason;
-const logs = banGuild.channels.find("name", "mod_log");
-if(!logs)
-{
-	console.log("Could not find channel.\n"+logs);
-	return;
-}
+	const logs = banGuild.channels.find("name", "mod_log");
+	if(!logs)
+	{
+		console.log("Could not find channel.\n"+logs);
+		return;
+	}
 
-banGuild.fetchAuditLogs({limit: 1, type: "DELETE"})
-	.then((Audits) =>
-{
-	var ban = Audits.entries.find("target", banUser);
-// console.log(ban);
+	banGuild.fetchAuditLogs({limit: 1, type: "DELETE"})
+		.then((Audits) =>
+	{
+		var ban = Audits.entries.find("target", banUser);
+		console.log(ban);
 
-if(!ban.reason) banReason = config.noReason;
-else banReason = ban.reason;
+		if(!ban.reason) banReason = config.noReason;
+		else banReason = ban.reason;
 
-const banEmbed = new Discord.RichEmbed()
-//============================================================
-	.setAuthor("Ban Log", client.user.avatarURL)
-	.addField("Banned:", ban.target)
-	.addField("Reason:", banReason)
-	.addField("Responsible Mod:", ban.executor)
-	.setFooter("Time: "+ban.createdAt)
-	.setColor(config.redColor);
-//============================================================
+		const banEmbed = new Discord.RichEmbed()
+		//============================================================
+			.setAuthor("Ban Log", client.user.avatarURL)
+			.addField("Banned:", ban.target)
+			.addField("Reason:", banReason)
+			.addField("Responsible Mod:", ban.executor)
+			.setFooter("Time: "+ban.createdAt)
+			.setColor(config.redColor);
+		//============================================================
 
-logs.send(banEmbed);
-}, (reason) => {console.log("Ban log rejected because:\n"+reason);})
+		logs.send(banEmbed);
+	}, (reason) => {console.log("Ban audit logs could not be found because:\n"+reason);})
 })
 
 //Will activate on any user leaving, however I've limited it to "MEMBER_KICK"
 client.on("guildMemberRemove", (removeUser) =>
 {
 	var kickReason;
-// console.log(removeUser);
-const logs = removeUser.guild.channels.find("name", "mod_log");
-if(!logs)
-{
-	console.log("Could not find channel.\n"+logs);
-	return;
-}
+	// console.log(removeUser);
+	const logs = removeUser.guild.channels.find("name", "mod_log");
+	if(!logs)
+	{
+		console.log("Could not find channel.\n"+logs);
+		return;
+	}
 
-removeUser.guild.fetchAuditLogs({limit: 1, type: "DELETE"})
-	.then((Audits) =>
-{
-	if(Audits.action !== "MEMBER_KICK") return;
-// console.log(Audits);
-var kick = Audits.entries.find("target", removeUser.user);
+	removeUser.guild.fetchAuditLogs({limit: 1, type: "DELETE"})
+		.then((Audits) =>
+	{
+		if(Audits.action !== "MEMBER_KICK") return;
+		// console.log(Audits);
+		var kick = Audits.entries.find("target", removeUser.user);
 
-if(!kick.reason) kickReason = config.noReason;
-else kickReason = kick.reason;
+		if(!kick.reason) kickReason = config.noReason;
+		else kickReason = kick.reason;
 
-const kickEmbed = new Discord.RichEmbed()
-//===========================================================
-	.setAuthor("Kick Log:", client.user.avatarURL)
-	.addField("Kicked:", kick.target)
-	.addField("Reason:", kickReason)
-	.addField("Responsible Mod:", kick.executor)
-	.setFooter("Time: "+kick.createdAt)
-	.setColor(config.redColor);
-//===========================================================
+		const kickEmbed = new Discord.RichEmbed()
+		//===========================================================
+			.setAuthor("Kick Log:", client.user.avatarURL)
+			.addField("Kicked:", kick.target)
+			.addField("Reason:", kickReason)
+			.addField("Responsible Mod:", kick.executor)
+			.setFooter("Time: "+kick.createdAt)
+			.setColor(config.redColor);
+		//===========================================================
 
-logs.send(kickEmbed);
-}, (reason) => {console.log("Kick log rejected because:\n"+reason);})
+		logs.send(kickEmbed);
+	}, (reason) => {console.log("Kick audit logs could not be found because:\n"+reason);})
 })
 
 //Calls on a deleted message
@@ -242,7 +245,19 @@ logs.send(kickEmbed);
 // 	}, (reason) => {console.log("Delete log rejected because:\n"+reason);})
 // })
 
-client.on("error", (e) => {console.error(e);})
+client.on("error", (e) =>
+{
+	const errGuild = client.guilds.find("name", "HappyBappy");
+	console.error(e);
+	errGuild.channels.find("name", "error-log").send("**__Error:__**\n"+e);
+})
+
+client.on("warn", (w) =>
+{
+	const errGuild = client.guilds.find("name", "HappyBappy");
+	console.warn(w);
+	errGuild.channels.find("name", "error-log").send("**Warning:**\n"+w);
+})
 
 /*
 client.emojis.find("name", "robot");
