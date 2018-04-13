9b1b50cf3fef83fcc3db734921c37d0962a191e8
Bappy56 <33536703+Bappy56@users.noreply.github.com>
2018-04-12 23:44:14 -0500

    DERP DERP
    I should really make a checklist... Corrected hardcoded version number... SMH.

--- haste_bot_v0.4.3.js
+++ haste_bot_v0.4.3.js
@@ -6,7 +6,7 @@ const embed = new Discord.RichEmbed();
 var pre;
 var sentMsg;
 
-const version = "Bot version: 0.4.2 ***Alpha***";
+const version = "Bot version: 0.4.3 ***Alpha***";
 
 var afterhours = false;
 
